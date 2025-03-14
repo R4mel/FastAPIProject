8f596d1a3a2273feb5a732762e51e60060c2ca68
Ramel <dev_20@naver.com>
2025-03-15 00:34:10 +0900

    crud

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 
+import models
+from database import engine
+
+models.Base.metadata.create_all(bind=engine)
+
+from board import board_router
+
 app = FastAPI()
 
+app.include_router(board_router.app, tags=["board"])
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
