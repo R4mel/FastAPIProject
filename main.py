8e7d80ecca1c9dc5017d5fa4bd6c1e26b21e2a40
Ramel <dev_20@naver.com>
2025-03-15 13:17:53 +0900

    basic login

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 import models
 from database import engine
+from user import user_router
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -10,6 +11,7 @@ from board import board_router
 app = FastAPI()
 
 app.include_router(board_router.app, tags=["board"])
+app.include_router(user_router.app, tags=["user"])
 
 @app.get("/")
 async def root():
