c76f1990ba0f636e3d15afaa36755eb26830f9e9
Ramel <dev_20@naver.com>
2025-03-17 13:41:11 +0900

    main.py 수정

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import models
 from database import engine
 from user import user_router
 
-models.Base.metadata.create_all(bind=engine)
-
 from board import board_router
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(board_router.app, tags=["board"])
 app.include_router(user_router.app, tags=["user"])
+
 
 @app.get("/")
 async def root():
